6e9fb22d5ff45d520476c6e08aaaeba6ea7b5740
Michaela Mattes <michaela.mattes@t-systems.com>
2023-03-22 12:25:39 +0100

    update style

--- variables.tf
+++ variables.tf
@@ -6,20 +6,23 @@ variable "tpl_local_name" {
 
 locals {
   default = {
-    /** resource definition */
     tpl_local_name = {
       name = ""
       tags = {}
     }
   }
 
-  /** compare and merge custom and default values */
+  /**
+    compare and merge custom and default values
+  */
   tpl_local_name_values = {
     for tpl_local_name in keys(var.tpl_local_name) :
     tpl_local_name => merge(local.default.tpl_local_name, var.tpl_local_name[tpl_local_name])
   }
 
-  /** deep merge of all custom and default values */
+  /**
+    deep merge of all custom and default values
+  */
   tpl_local_name = {
     for tpl_local_name in keys(var.tpl_local_name) :
     tpl_local_name => merge(
