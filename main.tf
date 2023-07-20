0a95b501800aead4804405f626978167fc07d85f
Michaela Mattes <m.mattes@telekom.de>
2023-07-20 14:24:45 +0200

    add homepage setting

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 /**
 * # tpl_module
 *
-* This module manages the tpl_provider tpl_module resources.
-* For more information see https://registry.terraform.io/providers/tpl_provider/latest/docs > tpl_module
+* This module manages the tpl_provider tpl_module resources, see https://registry.terraform.io/providers/tpl_provider/latest/docs.
+* For more information about the module structure see https://telekom-mms.github.io/terraform-template.
 *
 */
 
