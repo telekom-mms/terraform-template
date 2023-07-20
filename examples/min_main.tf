93e832f81b9a6eb55c4a1c870e4445b88cfa7c8f
Michaela Mattes <m.mattes@telekom.de>
2023-07-20 09:57:59 +0200

    add gh-pages

--- examples/min_main.tf
+++
@@ -1,9 +0,0 @@
-module "tpl_module" {
-  source = "tpl_source"
-  tpl_local_name = {
-    tpl_name = {
-      location            = "westeurope"
-      resource_group_name = "rg-mms-github"
-    }
-  }
-}
